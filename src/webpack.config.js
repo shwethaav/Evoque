67c0ea0752e051ede3084ae96caf34ebfc4b63c7
shwetha . <94182880+shwethaav@users.noreply.github.com>
2024-06-15 19:04:25 +0530

    done

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,35 +1,32 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-            // Add any other Babel plugins or options as needed
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist')
-  }
-};
+// module.exports = {
+//   entry: './src/index.js', // Assuming your entry point is in src/index.js
+//   output: {
+//     path: path.resolve(__dirname, 'dist'), // Output to /path/to/your/project/dist
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader'
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       },
+//       {
+//         test: /\.(png|svg|jpg|gif)$/,
+//         use: ['file-loader']
+//       }
+//     ]
+//   },
+//   devServer: {
+//     contentBase: path.resolve(__dirname, 'dist'), // Serve content from /path/to/your/project/dist
+//     // other devServer configurations
+//   }
+// };

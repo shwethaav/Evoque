0cb0b101e8c7198a738040d41d7529909061ccd1
shwetha . <94182880+shwethaav@users.noreply.github.com>
2024-06-15 18:53:17 +0530

    pack changes done

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,31 +1,35 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',  
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),  
-    filename: 'bundle.js'  
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,  
-        exclude: /node_modules/,  
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'  
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+            // Add any other Babel plugins or options as needed
+          }
         }
       },
       {
-        test: /\.css$/,  
-        use: ['style-loader', 'css-loader']  /
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|svg|jpg|gif)$/, 
+        test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader']
       }
     ]
   },
   devServer: {
-    contentBase: './dist'  
+    contentBase: path.resolve(__dirname, 'dist')
   }
 };
